857f0bb036db53362e3d6da72ff285c935614585
barbaradecares <barbaradecares@hotmail.com>
2018-11-24 20:39:21 -0600

    minimun requirements done

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,33 @@
+const submitButton = document.querySelector('[value="Create New Task"]')
+const taskInput = document.querySelector('#new-task-description')
+const tasksList = document.querySelector('#tasks')
 
-document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
-});
+let allTasks = [];
+
+submitButton.addEventListener('click', function(e){
+  e.preventDefault()
+  
+      allTasks.push(taskInput.value)
+      taskInput.value = ''
+      render();
+})
+
+const render = function(){
+  tasksList.innerHTML = ``
+  allTasks.forEach(function(task){
+      tasksList.innerHTML += `
+          <li>${task}</li>`
+      
+    const deleteButton = document.createElement('button')
+    deleteButton.innerText = 'X'
+
+    deleteButton.addEventListener('click', function(event){
+      
+    let indexOfTask = allTasks.indexOf(task)
+    allTasks.splice(indexOfTask, 1)
+    render()
+    })
+  })  
+}
+
+render();
